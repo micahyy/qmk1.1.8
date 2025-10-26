1bc83f0f4dc3ca033189f96705e5b2976cce1c45
Micah <45813290+micahyy@users.noreply.github.com>
2025-10-26 17:34:24 +0800

    1026

---
+++ .build/obj_0mao_08gamerst_via/src/info_deps.d
@@ -0,0 +1,42 @@
+generated-files: $(wildcard keyboards/0mao/08gamerst/info.json)
+
+generated-files: $(wildcard keyboards/0mao/08gamerst/rules.mk)
+
+generated-files: $(wildcard keyboards/0mao/08gamerst/post_rules.mk)
+
+generated-files: $(wildcard keyboards/0mao/08gamerst/config.h)
+
+generated-files: $(wildcard keyboards/0mao/08gamerst/post_config.h)
+
+generated-files: $(wildcard keyboards/0mao/info.json)
+
+generated-files: $(wildcard keyboards/0mao/rules.mk)
+
+generated-files: $(wildcard keyboards/0mao/post_rules.mk)
+
+generated-files: $(wildcard keyboards/0mao/config.h)
+
+generated-files: $(wildcard keyboards/0mao/post_config.h)
+
+generated-files: $(wildcard C:/Users/mm/Desktop/qmk1.1.8/keyboards/0mao/08gamerst/keymaps/via/keymap.json)
+
+generated-files: $(wildcard C:/Users/mm/Desktop/qmk1.1.8/keyboards/0mao/08gamerst/keymaps/via/info.json)
+
+generated-files: $(wildcard C:/Users/mm/Desktop/qmk1.1.8/keyboards/0mao/08gamerst/keymaps/via/rules.mk)
+
+generated-files: $(wildcard C:/Users/mm/Desktop/qmk1.1.8/keyboards/0mao/08gamerst/keymaps/via/post_rules.mk)
+
+generated-files: $(wildcard C:/Users/mm/Desktop/qmk1.1.8/keyboards/0mao/08gamerst/keymaps/via/config.h)
+
+generated-files: $(wildcard C:/Users/mm/Desktop/qmk1.1.8/keyboards/0mao/08gamerst/keymaps/via/post_config.h)
+
+generated-files: $(wildcard users/via/info.json)
+
+generated-files: $(wildcard users/via/rules.mk)
+
+generated-files: $(wildcard users/via/post_rules.mk)
+
+generated-files: $(wildcard users/via/config.h)
+
+generated-files: $(wildcard users/via/post_config.h)
+
